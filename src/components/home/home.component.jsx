fd6d96d70652d698fa6a1070de49c34c8a318a8e
¨areyes107¨ <angelmrg2000@hotmail.com>
2023-02-24 17:32:34 -0600

    added manteinance info

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import qrCode from "../../assets/img/frame.png";
 
 export function Home() {
   return (
-    <div style={{ marginLeft: 3 }}>
-      <div>Estamos dándole mantenimiento al sitio web</div>
+    <div style={{ margin: "2vh", textAlign: "center" }}>
+      <div>
+        <p> Estamos trabajando para darte un mejor servicio </p>
+        <p>Mientras puedes solicitar tus productos vía Facebook </p>
+
+        <img
+          src={qrCode}
+          alt=""
+          style={{ marginLeft: "auto", marginRight: "auto", display: "block" }}
+        />
+        <a href="https://www.facebook.com/people/Ferremundo/100090276001449/">
+          Puedes escanear el código QR o haz click aquí para ir a nuestro
+          Facebook
+        </a>
+      </div>
     </div>
   );
 }
