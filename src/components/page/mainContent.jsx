a82b7857dc17fe93417c88ff6640f450e9b907b3
¨areyes107¨ <angelmrg2000@hotmail.com>
2023-02-28 23:39:47 -0600

    added a background full white

--- src/components/page/mainContent.jsx
+++ src/components/page/mainContent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function MainContent({ children }) {
-  return <div>{children}</div>;
+  return <div style={{ backgroundColor: "#ffffff" }}>{children}</div>;
 }
